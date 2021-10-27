9f824b39875e273617865731065049064a11cbd8
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2021-10-27 16:59:28 +0200

    Improve code efficiency using loop vectorization

--- 1-Data_Analysis/2-Statistical_Data_Analysis/Quiz_Detecting_Outliers.m
+++ 1-Data_Analysis/2-Statistical_Data_Analysis/Quiz_Detecting_Outliers.m
@@ -1,16 +1,30 @@
 load nodetraffic.mat;
 
 round(mean(nodetraffic))
-
 round(std(nodetraffic))
 
-count = 0;
+% Method I
+tic
+count1 = 0;
 for i = 1 : length(nodetraffic)
     if (standard_deviation_distance(nodetraffic, nodetraffic(i)) > 6)
-        count = count + 1;
+        count1 = count1 + 1;
     end
 end
-count
+count1
+toc
+
+%% Method II
+tic
+A = arrayfun(@(x) standard_deviation_distance(nodetraffic, x), nodetraffic);
+count2 = length(A(A > 6)) % Or: count2 = sum(A > 6)
+toc
+
+% Method III
+tic
+A = (nodetraffic - mean(nodetraffic)) ./ std(nodetraffic);
+count3 = sum(A > 6)
+toc
 
 % Answer
 % 1) 24
